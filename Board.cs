5060e9b1fd82c03517218357e079a7327c0ff475
xobre <mua.breana@gmail.com>
2019-10-20 16:06:07 -0700

    started creating the board

---
+++ Board.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace battleship
+{
+    class Board
+    {
+        private static int BOARD_DIMENSION = 10;
+        private int[,] PlayerOneGameBoard { get; set; }
+        public Board()
+        {
+            PlayerOneGameBoard = new int[BOARD_DIMENSION, BOARD_DIMENSION];
+        }
+
+    }
+}
